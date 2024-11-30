9974bec1b6c15e134a5e1d058558407e5d4c8f8f
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-30 16:58:08 +0600

    Connected with MongoDb

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ///import files
 const express = require('express');
 const app = express();
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -9,8 +9,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());  //
 
+///mongodb database
+
+const uri = "mongodb+srv://azizarrahman01032003:MKAr9ci87DBiIaRK@cluster0.yzj1q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+//===mongo db end
+
 app.get('/', (req, res) => {
-    res.send('GET request to the homepage')
+    res.send('GET request to the homepage');
 })
 
 app.listen(port, () => {
