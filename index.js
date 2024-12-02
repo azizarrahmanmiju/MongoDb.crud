2e9794bfb76409741de1ff5f9e26dd08ed213b0f
azizarrahman <azizarrahman01032003@gmail.com>
2024-12-02 17:33:39 +0600

    Delete User

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ///import files
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -23,13 +23,15 @@ const client = new MongoClient(uri, {
 
 const db = client.db("Users_Database");
 const dbcol = db.collection("Users");
-const getcoll = db.col
+let result;
 
 async function run() {
     try {
         await client.connect();
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        result = await dbcol.find().toArray();
+
 
     } finally {
         // await client.close();
@@ -43,14 +45,50 @@ app.post('/users', async (req, res) => {
     console.log(user)
     const result = await dbcol.insertOne(user);
     console.log(result, result.insertedIds)
-    res.send(result,);
+    res.send(result);
 });
 
 
 
+
 app.get('/', (req, res) => {
-    res.send('Server Is running');
+    res.send(result);
 })
+app.post('/user', async (req, res) => {
+    try {
+        const { name } = req.body;
+        if (!name) {
+            res.send("Please Provide Username")
+        }
+        const user = await dbcol.findOne({ name: name });
+        console.log(user);
+        res.send(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "Internal Server Error" });
+    }
+});
+app.delete('/users/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const query = { _id: new ObjectId(id) };
+    try {
+
+        const result = await dbcol.deleteOne(query);
+
+        if (result.deletedCount === 1) {
+            res.send("User deleted");
+        } else {
+            res.send("User not found");
+        }
+    } catch (err) {
+        res.status(500).send('Internal Server Error');
+    }
+
+
+});
+
+
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
